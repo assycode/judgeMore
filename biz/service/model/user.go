c52f5e9ff80990761370a281849642a57693b589
assycode <2518866325@qq.com>
2025-11-09 00:15:56 +0800

    Update user.go

--- biz/service/model/user.go
+++ biz/service/model/user.go
@@ -21,3 +21,28 @@ type EmailAuth struct {
 	Uid   string
 	Time  int64 //时间戳
 }
+type UpdateUserRequest struct {
+	UserId    int64
+	UserName  string
+	Email     string
+	Password  string
+	CollegeId int64
+	MajorId   int64
+	Grade     string
+}
+
+type UpdateMajorRequest struct {
+	MajorId   int64
+	CollegeId int64
+	MajorName string
+}
+
+type UpdateCollegeRequest struct {
+	CollegeId   int64
+	CollegeName string
+}
+type QueryUserRequest struct {
+	CollegeId int64
+	MajorId   int64
+	Role      string
+}
